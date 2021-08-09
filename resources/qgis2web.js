b3ecf60d3f69ba89de2418e6c960d73e68b0815a
Rophis <88201789+Rophis@users.noreply.github.com>
2021-08-09 09:15:57 -0400

    Add files via upload

--- resources/qgis2web.js
+++ resources/qgis2web.js
@@ -77,7 +77,7 @@ var layerSwitcher = new ol.control.LayerSwitcher({tipLabel: "Layers"});
 map.addControl(layerSwitcher);
 
     var searchLayer = new SearchLayer({
-      layer: lyr_BOL_DEP_17,
+      layer: lyr_BOL_DEP_20,
       colName: 'NOM_DEP',
       zoom: 10,
       collapsed: true,
